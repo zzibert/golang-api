5d72dc7da91b46d0933cab2510294a3d5874a65a
zan <zan_zibert@hotmail.com>
2019-06-15 18:21:48 +0200

    learning from tutorial

--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
-import "os"
-
 func main() {
 	a := App{}
-	a.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"),
-	)
+	a.Initialize()
+
 	a.Run(":8080")
 }
