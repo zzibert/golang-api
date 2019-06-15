d4df4aac5bb653a96d8cc3c62362777ffca36500
zan <zan_zibert@hotmail.com>
2019-06-15 16:33:59 +0200

    adding product methods and tests

---
+++ main_test.go
@@ -0,0 +1,26 @@
+// main_test.go
+
+package main_test
+
+import (
+	"os"
+	"testing"
+)
+
+var a main.App
+
+func TestMain(m *testing.M) {
+	a = main.App{}
+	a.Initialize(
+		os.Getenv("TEST_DB_USERNAME"),
+		os.Getenv("TEST_DB_PASSWORD"),
+		os.Getenv("TEST_DB_NAME"))
+
+	ensureTableExists()
+
+	code := m.Run()
+
+	clearTable()
+
+	os.Exit(code)
+}
